fe42dff3bc4b31c5c02c93899624b382e44ebde6
joeyrobinson <joeyrobinsondev@gmail.com>
2018-05-23 17:47:04 -0500

    almost

--- src/Components/Officers/OfficerModals/AvenOfficerModal.js
+++ src/Components/Officers/OfficerModals/AvenOfficerModal.js
@@ -4,7 +4,7 @@ const AvenOfficerModal = (props) => (
   <div className="ModalText">
     <button className="CloseModalButton" onClick={props.onClick}>
     <h1>Hi, I'm Aven.</h1>
-      <p>Hi I'm Aven. I help organize social events for the FC and I play SMN. Please ask if you have any questions and feel free to drop by the fc house for a visit!</p>
+      <p>Hi I'm Aven. I help organize social events for the FC and I play SMN. Please ask if you have any questions and feel free to drop by the fc house for a visit! Also, I love cute stuffed animals, and cute in-game pets XD</p>
     </button>
   </div>
 );
--- src/Components/Officers/OfficerModals/JoeyOfficerModal.js
+++ src/Components/Officers/OfficerModals/JoeyOfficerModal.js
@@ -4,7 +4,7 @@ const JoeyOfficerModal = (props) => (
   <div className="ModalText">
     <button className="CloseModalButton" onClick={props.onClick}>
     <h1>Hi, I'm Joey.</h1>
-      <p>Hello. I'm supposed to fill this out, but I'm putting this here. No one will ever notice. I like raiding and gathering. If you need any help feel free to ask. Oh, I made this website.</p>
+      <p>Hello. I'm supposed to fill this out, but I'm putting this here. No one will ever notice. I like raiding and gathering. If you need any help feel free to ask. Oh, I made this website. And I like the Lakshmi theme song a lot.</p>
     </button>
   </div>
 );
